40f8b875805606a216a7d1c6a2eb0dbd7fde9f66
merci <merciana.goncalves@gmail.com>
2021-04-11 22:13:05 -0300

    Tetris, foi o que consegui realizar de início

---
+++ app/src/main/java/com/example/testelogin/ActivityJogo.kt
@@ -0,0 +1,16 @@
+package com.example.testelogin
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil.setContentView
+
+class ActivityJogo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        setContentView ( R .layout.activity_jogo)
+
+        val params = intent.extras
+    }
+}
